b05a498eb595aeb83a6145d77f7fe9eb0be48f9d
Brendon Smith <br3ndonland@protonmail.com>
2020-08-17 16:12:05 -0400

    Add logging configuration function to start.py
    This commit will add start.py `configure_logging()`. The function accepts
    a `Path()` to a logging configuration file, which can be either .py,
    .yml, .yaml, .conf, or .ini.
    
    For .py files, the function will load the Python file as a module, load
    the `LOGGING_CONFIG` dictionary object, and pass it to
    `logging.config.dictConfig()`.
    
    YAML files are opened with a context manager. PyYAML parses the YAML, and
    then passes the parsed YAML dictionary to `logging.config.dictConfig()`.
    
    The old-style .conf, or .ini. files are loaded with
    `logging.config.fileConfig()`.
    
    The type: ignore mypy comment prevents the following error messages,
    related to `spec.loader.exec_module()`:
    
    - Item "_Loader" of "Optional[_Loader]" has no attribute "exec_module"
      [union-attr] mypy(error)
    - Item "None" of "Optional[_Loader]" has no attribute "exec_module"
      [union-attr] mypy(error)
    
    https://mypy.readthedocs.io/en/latest/error_code_list.html

--- inboard/start.py
+++ inboard/start.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+import importlib.util
+import logging.config
 import os
 import subprocess
 from pathlib import Path
 
 import uvicorn  # type: ignore
+import yaml
 
 
 def set_app_module() -> str:
@@ -34,6 +37,40 @@ def set_conf_path(module: str) -> Path:
         conf_path = f"/{module}_conf.py"
     os.environ[f"{module.upper()}_CONF"] = conf_path
     return Path(conf_path)
+
+
+def configure_logging(logging_conf: Path = Path("/logging_conf.py")) -> str:
+    """Configure Python logging based on a path to a logging configuration file."""
+    try:
+        if logging_conf.suffix == ".py":
+            spec = importlib.util.spec_from_file_location("confspec", logging_conf)
+            logging_conf_module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(logging_conf_module)  # type: ignore
+            if getattr(logging_conf_module, "LOGGING_CONFIG"):
+                logging_conf_dict = getattr(logging_conf_module, "LOGGING_CONFIG")
+            else:
+                raise AttributeError(f"No LOGGING_CONFIG in {logging_conf_module}.")
+            if isinstance(logging_conf_dict, dict):
+                logging.config.dictConfig(logging_conf_dict)
+                message = f"Logging dict config loaded from {logging_conf}."
+            else:
+                raise TypeError("LOGGING_CONFIG is not a dictionary instance.")
+        elif logging_conf.suffix in [".yml", ".yaml"]:
+            with open(logging_conf, "r") as file:
+                logging_conf_file = file.read()
+                yaml_contents = yaml.load(logging_conf_file, Loader=yaml.SafeLoader)
+                logging.config.dictConfig(yaml_contents)
+                message = f"Logging dict config loaded from YAML in {logging_conf}."
+        elif logging_conf.suffix in [".conf", ".ini"]:
+            logging.config.fileConfig(logging_conf, disable_existing_loggers=False)
+            message = f"Logging file config loaded from {logging_conf}."
+        else:
+            raise ImportError(f"Unable to configure logging with {logging_conf.name}.")
+    except Exception as e:
+        message = f"Error when configuring logging: {e}"
+    # TODO: print statements will be replaced by logger messages
+    print(message)
+    return message
 
 
 def run_pre_start_script(
@@ -77,6 +114,7 @@ if __name__ == "__main__":
     app_module = set_app_module()
     gunicorn_conf_path = set_conf_path("gunicorn")
     logging_conf_path = set_conf_path("logging")
+    configure_logging(logging_conf=logging_conf_path)
     run_pre_start_script()
     start_server(
         app_module=app_module, gunicorn_conf=gunicorn_conf_path,
