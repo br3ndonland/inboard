9e352df1da9638ded949509a0a307abbfef02f80
Brendon Smith <br3ndonland@protonmail.com>
2020-08-20 16:41:38 -0400

    Add CORS and Basic Auth endpoints to app modules
    - Add CORS allowed origins
    - Add FastAPI and Starlette app endpoints with HTTP Basic Auth

--- inboard/app/fastapibase/main.py
+++ inboard/app/fastapibase/main.py
@@ -2,15 +2,41 @@ import os
 import sys
 from typing import Dict
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from inboard.app.utilities import basic_auth
 
 server = "Uvicorn" if bool(os.getenv("WITH_RELOAD")) else "Uvicorn, Gunicorn"
 version = f"{sys.version_info.major}.{sys.version_info.minor}"
 
-app = FastAPI()
+app = FastAPI(title="inboard")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="https://br3ndon.land",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
-async def root() -> Dict[str, str]:
+async def get_root() -> Dict[str, str]:
+    return {"Hello": "World"}
+
+
+@app.get("/health")
+def get_health(auth: str = Depends(basic_auth)) -> Dict[str, str]:
+    return {"application": app.title, "status": "active"}
+
+
+@app.get("/status")
+def get_status(auth: str = Depends(basic_auth)) -> Dict[str, str]:
     message = f"Hello World, from {server}, FastAPI, and Python {version}!"
-    return {"message": message}
+    return {"application": app.title, "status": "active", "message": message}
+
+
+@app.get("/users/me")
+def get_current_user(username: str = Depends(basic_auth)) -> Dict[str, str]:
+    return {"username": username}
--- inboard/app/starlettebase/main.py
+++ inboard/app/starlettebase/main.py
@@ -2,16 +2,49 @@ import os
 import sys
 
 from starlette.applications import Starlette
+from starlette.authentication import requires
+from starlette.middleware.authentication import AuthenticationMiddleware
+from starlette.middleware.cors import CORSMiddleware
 from starlette.requests import Request
 from starlette.responses import JSONResponse
+
+from inboard.app.utilities import BasicAuth
 
 server = "Uvicorn" if bool(os.getenv("WITH_RELOAD")) else "Uvicorn, Gunicorn"
 version = f"{sys.version_info.major}.{sys.version_info.minor}"
 
 app = Starlette()
+app.add_middleware(AuthenticationMiddleware, backend=BasicAuth())
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="https://br3ndon.land",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.route("/")
-async def homepage(request: Request) -> JSONResponse:
+async def get_root(request: Request) -> JSONResponse:
+    return JSONResponse({"Hello": "World"})
+
+
+@app.route("/health")
+@requires("authenticated")
+def get_health(request: Request) -> JSONResponse:
+    return JSONResponse({"application": "inboard", "status": "active"})
+
+
+@app.route("/status")
+@requires("authenticated")
+def get_status(request: Request) -> JSONResponse:
     message = f"Hello World, from {server}, Starlette, and Python {version}!"
-    return JSONResponse({"message": message})
+    return JSONResponse(
+        {"application": "inboard", "status": "active", "message": message}
+    )
+
+
+@app.route("/users/me")
+@requires("authenticated")
+def get_current_user(request: Request) -> JSONResponse:
+    return JSONResponse({"username": request.user.display_name})
