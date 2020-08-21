ffbfd5ea8fe75d622f2084044f4d55a35c1d3aef
Brendon Smith <br3ndonland@protonmail.com>
2020-08-21 01:45:39 -0400

    Add Pytest fixture for HTTP Basic Auth credentials
    https://docs.pytest.org/en/stable/fixture.html

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import logging
+import os
 from typing import List
 
 import pytest  # type: ignore
+from _pytest.monkeypatch import MonkeyPatch  # type: ignore
 from fastapi.testclient import TestClient
 from pytest_mock import mocker  # type: ignore
 
@@ -12,13 +14,27 @@ from inboard.app.starlettebase.main import app as starlette_app
 
 @pytest.fixture
 def clients() -> List[TestClient]:
+    """Instantiate test client classes."""
     return [TestClient(fastapi_app), TestClient(starlette_app)]
 
 
 @pytest.fixture
+def basic_auth(
+    monkeypatch: MonkeyPatch,
+    username: str = "test_username",
+    password: str = "test_password",
+) -> tuple:
+    """Set username and password for HTTP Basic Auth."""
+    monkeypatch.setenv("BASIC_AUTH_USERNAME", username)
+    monkeypatch.setenv("BASIC_AUTH_PASSWORD", password)
+    assert os.getenv("BASIC_AUTH_USERNAME") == username
+    assert os.getenv("BASIC_AUTH_PASSWORD") == password
+    return username, password
+
+
+@pytest.fixture
 def mock_logger(mocker: mocker) -> logging.Logger:
-    """Mock the logger with pytest-mock and a pytest fixture
-    ---
+    """Mock the logger with pytest-mock and a pytest fixture.
     - https://github.com/pytest-dev/pytest-mock
     - https://docs.pytest.org/en/latest/fixture.html
     """
