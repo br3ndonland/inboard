a3ffa71113d93e30ea96af00f4aab345be2cdd07
Brendon Smith <br3ndonland@protonmail.com>
2020-08-20 23:19:48 -0400

    Add Starlette Basic Auth middleware to utilities
    The documentation was not helpful for adding HTTP Basic Auth. I based my
    middleware on starlette/tests/test_authentication.py instead.

--- inboard/app/utilities.py
+++ inboard/app/utilities.py
@@ -1,8 +1,36 @@
+import base64
+import binascii
 import os
 from secrets import compare_digest
+from typing import Tuple, Union
 
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from starlette.authentication import (
+    AuthCredentials,
+    AuthenticationBackend,
+    AuthenticationError,
+    SimpleUser,
+)
+from starlette.requests import HTTPConnection
+
+
+class BasicAuth(AuthenticationBackend):
+    async def authenticate(
+        self, request: HTTPConnection
+    ) -> Union[Tuple[AuthCredentials, SimpleUser], None]:
+        if "Authorization" not in request.headers:
+            return None
+
+        auth = request.headers["Authorization"]
+        try:
+            scheme, credentials = auth.split()
+            decoded = base64.b64decode(credentials).decode("ascii")
+        except (ValueError, UnicodeDecodeError, binascii.Error):
+            raise AuthenticationError("Invalid basic auth credentials")
+
+        username, _, password = decoded.partition(":")
+        return AuthCredentials(["authenticated"]), SimpleUser(username)
 
 
 def basic_auth(credentials: HTTPBasicCredentials = Depends(HTTPBasic())) -> str:
