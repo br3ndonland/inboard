2a56650016b9a8d5a5643cdc14681edcca14996f
Brendon Smith <br3ndonland@protonmail.com>
2020-09-13 00:28:03 -0400

    Increase test coverage of inboard/__init__.py
    71% -> 100%

--- inboard/__init__.py
+++ inboard/__init__.py
@@ -5,10 +5,10 @@ inboard
 from importlib.metadata import version
 
 
-def package_version() -> str:
+def package_version(package: str = __package__) -> str:
     """Calculate version number based on pyproject.toml"""
     try:
-        return version(__package__)
+        return version(package)
     except Exception:
         return "Package not found."
 
--- tests/test_version.py
+++ tests/test_version.py
@@ -1,5 +1,18 @@
-from inboard import __version__
+from inboard import __version__, package_version
+
+current_version = "0.4.1"
+
+
+def test_package_version() -> None:
+    """Test package version calculation."""
+    assert package_version() == current_version
+
+
+def test_package_version_not_found() -> None:
+    """Test package version calculation when package is not installed."""
+    assert package_version(package="incorrect") == "Package not found."
 
 
 def test_version() -> None:
-    assert __version__ == "0.4.1"
+    """Test package version number."""
+    assert __version__ == current_version
