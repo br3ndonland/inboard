fabc7fc8dc7634c09d0c586bc6509a1f55aa3b0e
Brendon Smith <br3ndonland@protonmail.com>
2020-11-26 14:05:24 -0500

    Fix test of custom Gunicorn worker calculations
    This commit will update `test_gunicorn_conf_workers_custom_cores` to
    correct the custom Gunicorn worker number calculation.
    
    The test failure occurred on my new Apple Silicon M1 Mac mini. I didn't
    see this test failure before, because I've never had 8 CPU cores before!
    
    Test failure below:
    
    TestConfigureGunicorn.test_gunicorn_conf_workers_custom_cores
    
    self = <test_start.TestConfigureGunicorn object at 0x10d2c3d90>
    monkeypatch = <_pytest.monkeypatch.MonkeyPatch object at 0x10d2c3bb0>
    
        def test_gunicorn_conf_workers_custom_cores(self, monkeypatch: MonkeyPatch) -> None:
            """Test custom Gunicorn worker process calculation."""
            monkeypatch.setenv("WORKERS_PER_CORE", "0.5")
            assert os.getenv("WORKERS_PER_CORE") == "0.5"
            cores: int = multiprocessing.cpu_count()
    >       assert gunicorn_conf.calculate_workers(
                None, "2", str(os.getenv("WORKERS_PER_CORE")), cores=cores
            ) == max(int(cores * 0.5), 2)
    E       AssertionError: assert 2 == 4
    E        +  where 2 = <function calculate_workers at 0x10cff4430>(None, '2', '0.5', cores=8)
    E        +    where <function calculate_workers at 0x10cff4430> = gunicorn_conf.calculate_workers
    E        +    and   '0.5' = str('0.5')
    E        +      where '0.5' = <function getenv at 0x10b60eca0>('WORKERS_PER_CORE')
    E        +        where <function getenv at 0x10b60eca0> = os.getenv
    E        +  and   4 = max(4, 2)
    E        +    where 4 = int((8 * 0.5))
    
    tests/test_start.py:95: AssertionError

--- tests/test_start.py
+++ tests/test_start.py
@@ -2,7 +2,7 @@ import logging
 import multiprocessing
 import os
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
@@ -87,14 +87,37 @@ class TestConfigureGunicorn:
             == int(number_of_workers)
         )
 
-    def test_gunicorn_conf_workers_custom_cores(self, monkeypatch: MonkeyPatch) -> None:
-        """Test custom Gunicorn worker process calculation."""
+    @pytest.mark.parametrize("concurrency", [None, "10"])
+    def test_gunicorn_conf_workers_custom_cores(
+        self, monkeypatch: MonkeyPatch, concurrency: Optional[str]
+    ) -> None:
+        """Test custom Gunicorn worker process calculation.
+        - Assert that number of workers equals `WORKERS_PER_CORE`, and is at least 2.
+        - Assert that setting `WEB_CONCURRENCY` overrides `WORKERS_PER_CORE`.
+        """
         monkeypatch.setenv("WORKERS_PER_CORE", "0.5")
-        assert os.getenv("WORKERS_PER_CORE") == "0.5"
+        workers_per_core = os.getenv("WORKERS_PER_CORE")
+        assert workers_per_core == "0.5"
         cores: int = multiprocessing.cpu_count()
         assert gunicorn_conf.calculate_workers(
-            None, "2", str(os.getenv("WORKERS_PER_CORE")), cores=cores
-        ) == max(int(cores * 0.5), 2)
+            None, None, workers_per_core, cores=cores
+        ) == max(int(cores * float(workers_per_core)), 2)
+        assert (
+            gunicorn_conf.calculate_workers(None, "10", workers_per_core, cores=cores)
+            == 10
+        )
+        monkeypatch.setenv("WEB_CONCURRENCY", concurrency) if concurrency else None
+        assert os.getenv("WEB_CONCURRENCY") == concurrency
+        assert (
+            (
+                gunicorn_conf.calculate_workers(
+                    None, concurrency, workers_per_core, cores=cores
+                )
+                == 10
+            )
+            if concurrency
+            else max(int(cores * float(workers_per_core)), 2)
+        )
 
 
 class TestConfigureLogging:
