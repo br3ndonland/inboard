94879d41cba53f22a2fdc86adc0b407196b1b163
Brendon Smith <br3ndonland@protonmail.com>
2021-04-19 18:53:54 -0400

    Separate FastAPI and Starlette imports for __all__
    br3ndonland/inboard#20
    br3ndonland/inboard#32
    br3ndonland/inboard#33
    
    This commit will refine the imports in inboard/`__init__.py` so that
    FastAPI and Starlette are imported separately. This way, if Starlette is
    installed but FastAPI is not, the HTTP Basic auth implementation for
    Starlette can still be imported and added to `__all__`.

--- inboard/__init__.py
+++ inboard/__init__.py
@@ -4,12 +4,14 @@ Docker images and utilities to power your Python APIs and help you ship faster.
 
 https://github.com/br3ndonland/inboard
 """
+# FastAPI and Starlette are optional, and will raise ImportErrors if not installed.
 try:
-    # FastAPI and Starlette are optional dependencies
     from .app.utilities_fastapi import basic_auth as fastapi_basic_auth
+except ImportError:  # pragma: no cover
+    pass
+try:
     from .app.utilities_starlette import BasicAuth as StarletteBasicAuth
 except ImportError:  # pragma: no cover
-    # ImportError exceptions will be raised if optional dependencies are not installed
     pass
 from .logging_conf import LOGGING_CONFIG, configure_logging
 
